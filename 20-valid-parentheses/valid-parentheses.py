106d647c72eafe039b487ed075b11a01f33926da
Gaurang <67051548+Gaurangn127@users.noreply.github.com>
2025-08-23 16:59:47 +0530

    Time: 3 ms (22.11%) | Memory: 17.9 MB (32.05%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # using data structure: stack 
+        stack = []
+
+        if len(s)%2 == 1 : return False
+        if s[0] in ')}]': return False
+
+        for i in range(len(s)) :
+            if s[i] in '({[':
+                stack.append(s[i])
+
+            # stack can be empty, in which case stack[-1] gives an index error
+            # which is why we first check if stack as opening bracket
+
+            elif (s[i] == ')') and ('(' in stack):
+                if stack[-1] == '(':
+                    stack.pop()
+            
+            elif (s[i] == '}') and ('{' in stack):
+                if stack[-1] == '{':
+                    stack.pop()
+
+            elif (s[i] == ']') and ('[' in stack):
+                if stack[-1] == '[':
+                    stack.pop()
+            
+            else :
+                return False
+        
+        if stack != []:
+            return False
+
+        return True
