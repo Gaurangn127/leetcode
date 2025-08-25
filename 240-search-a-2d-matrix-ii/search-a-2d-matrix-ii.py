58057517055f889709b06d5fb23be5dce4dadf60
Gaurang <67051548+Gaurangn127@users.noreply.github.com>
2025-08-25 18:19:26 +0530

    Time: 143 ms (65.26%) | Memory: 23.84 MB (96.37%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m = len(matrix) 
         n = len(matrix[0])
-        i, j = 0, n - 1
+        i, j = 0, n - 1         #using the top-right approach to find the desired element
 
         while i < m and j >= 0 :
             if target > matrix[i][j] :
