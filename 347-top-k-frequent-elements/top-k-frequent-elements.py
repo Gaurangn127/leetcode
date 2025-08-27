b354f24082c698b4d3a1f84a95fe8ab10f9f234a
Gaurang <67051548+Gaurangn127@users.noreply.github.com>
2025-08-28 00:45:35 +0530

    Time: 11 ms (22.91%) | Memory: 24.1 MB (24.40%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -13,9 +13,8 @@ class Solution:
   
         res = []
   
-        for i in range(len(freq)-1, 0, -1) :
+        for i in range(len(freq)-1, -1, -1) :
             for n in freq[i] :
                 res.append(n)
-  
-            if len(res) == k :
-                return res
+                if len(res) == k :
+                    return res
