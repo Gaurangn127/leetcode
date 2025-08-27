995710e56a047d9f47d66f96657f98312b999fdb
Gaurang <67051548+Gaurangn127@users.noreply.github.com>
2025-08-27 22:52:03 +0530

    Time: 1191 ms (88.24%) | Memory: 56.4 MB (48.18%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -7,8 +7,6 @@ class Solution:
         is_prime = [True] * n
         is_prime[0], is_prime[1] = False, False
 
-        count = 0
-
         for i in range(2, int(n ** 0.5) + 1) :
             if is_prime[i] :
 
