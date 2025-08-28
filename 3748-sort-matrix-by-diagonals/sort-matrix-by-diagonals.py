9a43aaaf9dc6fb9ae37dc1b530abf44e5232fe38
Gaurang <67051548+Gaurangn127@users.noreply.github.com>
2025-08-28 21:58:55 +0530

    Time: 11 ms (68.33%) | Memory: 17.87 MB (52.08%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -2,17 +2,20 @@ class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
         diagonals = defaultdict(list)
-
+        
+        # group diagonals together
         for i in range(n):
             for j in range(n):
                 diagonals[i - j].append(grid[i][j])
 
+        # sort diagonals
         for key in diagonals :
             if key < 0 :
                 diagonals[key].sort()
             else :
                 diagonals[key].sort(reverse = True)
 
+        # put diagonals back together
         for i in range(n) :
             for j in range(n) :
                 grid[i][j] = diagonals[i-j].pop(0)
